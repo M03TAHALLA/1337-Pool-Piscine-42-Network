8cd7f1b954e40bf9ca541c1fc718cb042bfdf403
MOHAMMED TAHALLA <111022553+SimoAs@users.noreply.github.com>
2024-06-12 22:33:41 +0100

    C00

---
+++ C00/ex07/ft_putnbr_main.c
@@ -0,0 +1,34 @@
+#include<unistd.h>
+
+void ft_putnbr(int nb){
+    long nbl ;
+    nbl = nb ;
+    char c[9];
+    int i = 0;
+    if(nb < 0){
+        write(1,"-",1);
+        nbl *= -1 ;
+    }
+    if(nbl == 0){
+        write(1,"0",1);
+    }
+    while (nbl > 0)
+    {
+        c[i++] = (nbl % 10) + '0';
+        nbl /= 10;
+    }
+    while ( i > 0)
+    {
+        write(1,&c[--i],1);
+    }
+    
+}
+
+void main(){
+    ft_putnbr(145);
+    write(1," , ",3);
+    ft_putnbr(-4);
+    write(1," , ",3);
+    ft_putnbr(0);
+
+}
