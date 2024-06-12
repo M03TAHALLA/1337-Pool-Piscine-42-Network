acb45bb4ecfe484f09303fa59d43accd74213233
MOHAMMED TAHALLA <111022553+SimoAs@users.noreply.github.com>
2024-06-12 23:12:17 +0100

    C00

---
+++ C01/ft_ft_main.c
@@ -0,0 +1,35 @@
+#include<unistd.h>
+void ft_putnbr(int nb){
+    long nbl ;
+    nbl = nb ;
+    char c[9];
+    int i = 0;
+    if(nb < 0){
+        write(1,"-",1);
+        nbl *= -1 ;
+    }
+    if(nbl == 0){
+        write(1,"0",1);
+    }
+    while (nbl > 0)
+    {
+        c[i++] = (nbl % 10) + '0';
+        nbl /= 10;
+    }
+    while ( i > 0)
+    {
+        write(1,&c[--i],1);
+    }
+    
+}
+void ft_ft(int *nbr){
+    *nbr = 42 ;
+}
+
+void main(){
+    int a = 5 ;
+    ft_putnbr(a);
+    ft_ft(&a);
+    write(1," -> ",4);
+    ft_putnbr(a);
+}
