19ed203ddad7460ea35cc290e414d8c95af89576
MOHAMMED TAHALLA <111022553+SimoAs@users.noreply.github.com>
2024-06-12 22:16:56 +0100

    C00

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -8,30 +8,27 @@ void ft_print_comb2(void){
 
     while (a <= '9')
     {
-        b = a + 1 ;
         while (b <= '8')
         {
-            c = b + 1;
             while (c <= '9')
             {
-                d = c + 1 ;
                 while (d <= '9')
                 {
                     write(1,&a,1);
                     write(1,&b,1);
                     write(1,&c,1);
                     write(1,&d,1);
-                    if( a != '9' ){
-                        write(1,", ",1);
-                    }
+                    write(1,", ",1);
                     d++;
                 }
                 c++;
-                
+                d = '1';
             }
-             b++;
+            b++;
+            c = '0';
         }
         a++;
+        b = '0';
     }
     
 }
