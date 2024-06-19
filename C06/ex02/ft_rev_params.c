5a6a9e84d1b29b78d0da78f5fe5e9819bd1d6fa9
MOHAMMED TAHALLA <111022553+SimoAs@users.noreply.github.com>
2024-06-19 23:21:21 +0100

    C06

--- C06/ex02/ft_rev_params.c
+++ C06/ex02/ft_rev_params.c
@@ -16,8 +16,7 @@ int main(int argc, char *argv[])
         j = 0 ;
         while (argv[i][j] != '\0')
     {
-        ft_putchar(argv[i][j]);
-        j++;
+        
     }
     ft_putchar('\n');
     i--;
